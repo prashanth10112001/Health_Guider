ebbe3cc80458de44d08af31859e4eb7115bdccdd
prashanth10112001 <prashanthdurgam1011@gmail.com>
2025-10-31 10:50:53 +0530

    CHanged some config files.

--- client/src/api/firebaseConfig.js
+++ client/src/api/firebaseConfig.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 
-
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyC6KE-DrDKHHl8PcDDDMDlvWBVhnYCzbv4",
-  authDomain: "mtech-project-a7835.firebaseapp.com",
-  projectId: "mtech-project-a7835",
-  storageBucket: "mtech-project-a7835.firebasestorage.app",
-  messagingSenderId: "17005197841",
-  appId: "1:17005197841:web:881b65c1c24573c1860fb0",
-  measurementId: "G-G2CEPTSMYL"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
